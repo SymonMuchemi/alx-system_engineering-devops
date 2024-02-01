98c83ba1e62f5837c77dffe2cb5e6aef0662e921
symonmuchemi <simonkenya846@gmail.com>
2024-02-01 05:33:15 +0300

    has capital letters

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
 # regular expression that matches a 10 digit number
+# must contain only numbers
 
 puts ARGV[0].scan(/^[\d]{10}$/).join
---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# regex that matches only capital letters
+
+puts ARGV[0].scan(/[A-Z]+/).join
