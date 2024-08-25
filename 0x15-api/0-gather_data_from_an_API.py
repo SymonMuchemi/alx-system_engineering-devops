2b7a0d2415fd54350aa7d5f5467631f3b8f3aae1
symonmuchemi <simonkenya846@gmail.com>
2024-08-25 17:23:05 +0300

    update: codestyle rules

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,6 +5,7 @@ place holder
 import requests
 from sys import argv
 
+
 def fetch_data(url):
     """get data from url
 
@@ -16,6 +17,7 @@ def fetch_data(url):
     """
     response = requests.get(url)
     return response.json()
+
 
 def get_username(base_url, user_id):
     """get the username
@@ -29,8 +31,9 @@ def get_username(base_url, user_id):
     """
     url = f'{base_url}/users?id={user_id}'
     user_data = fetch_data(url)
-    
+
     return user_data[0]['name'] if user_data else None
+
 
 def get_completed_tasks(base_url, user_id):
     """get all tasks completed by the user
@@ -45,6 +48,7 @@ def get_completed_tasks(base_url, user_id):
     url = f'{base_url}/todos?userId={user_id}&completed=true'
     return fetch_data(url)
 
+
 def get_total_task_count(base_url, user_id):
     """get the total number of tasks done by a user
 
@@ -57,8 +61,9 @@ def get_total_task_count(base_url, user_id):
     """
     url = f'{base_url}/todos?userId={user_id}'
     all_tasks = fetch_data(url)
-    
+
     return len(all_tasks)
+
 
 def print_task_summary(name, completed_tasks, total_tasks):
     """print a summary of a user's completed tasks
@@ -70,7 +75,7 @@ def print_task_summary(name, completed_tasks, total_tasks):
     """
     completed_len = len(completed_tasks)
     todo_list = []
-    
+
     for task in completed_tasks:
         todo_list.append(task['title'])
     print(f'Employee {name} is done with tasks({completed_len}/{total_tasks})')
@@ -81,16 +86,16 @@ def print_task_summary(name, completed_tasks, total_tasks):
 if __name__ == "__main__":
     if len(argv) < 2:
         exit()
-        
+
     base_url = 'https://jsonplaceholder.typicode.com'
     user_id = argv[1]
-    
+
     name = get_username(base_url, user_id)
     if not name:
         print('User not found')
         exit()
-    
+
     completed_tasks = get_completed_tasks(base_url, user_id)
     total_tasks = get_total_task_count(base_url, user_id)
-    
+
     print_task_summary(name, completed_tasks, total_tasks)
