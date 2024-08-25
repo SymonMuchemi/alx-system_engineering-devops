50e23fa83636e69217552bd39ee9335a7d810900
symonmuchemi <simonkenya846@gmail.com>
2024-08-25 20:08:08 +0300

    ignore header to match test script output

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -102,7 +102,7 @@ def write_to_csv(base_url, user_id):
 
     with open(file_name, mode='w+', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=fields)
-        writer.writeheader()
+        # writer.writeheader()
 
         writer.writerows(data)
 
