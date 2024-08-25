47545a9fb0f8aafbe93fca716acfe7ee52ee8e75
symonmuchemi <simonkenya846@gmail.com>
2024-08-26 01:56:53 +0300

    include headers and check response code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,14 +16,14 @@ def number_of_subscribers(subreddit):
     base_url = f'https://www.reddit.com/r/{subreddit}/about.json'
     response = requests.get(
         base_url,
-        # headers={
-        #     'User-Agent': 'My-User-Agent'
-        #     },
+        headers={
+            'User-Agent': 'My-User-Agent'
+            },
         allow_redirects=False)
     
     # print(response.status_code)
 
-    if response.status_code == 404:
+    if response.status_code >= 300:
         return 0
 
     subs = response.json()
