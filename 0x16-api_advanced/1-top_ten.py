fdbdf8e6d4ac561c2bf08af4d59136318679261d
symonmuchemi <simonkenya846@gmail.com>
2024-09-01 00:33:50 +0300

    task: get top ten posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Quaries the Reddit API to get the titles of
+the first 10 posts lidted for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    response = requests.get(
+        f'https://www.reddit.com/r/{subreddit}/hot.json',
+        headers={'User-Agent': 'Custon'},
+        params={'limit': 10}
+    )
+    
+    if response.status_code == 200:
+        data = response.json().get('data').get('children')
+        for item in data:
+            print(item.get('data').get('title'))
+    else: 
+        print(None)
